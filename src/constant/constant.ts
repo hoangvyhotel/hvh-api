4fce4e718c380d359d9957a47da6835265562c6d
DatNT <datnguyen151004@gmail.com>
2025-08-27 20:18:28 +0700

    add booking logic

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,2 +1,18 @@
-export const VALID_STATUSES = ['ACT', 'INA', 'ACTG', 'PND', 'BAN', 'DEL', 'EXP', 'REV'] as const;
-export type CodeStatus = typeof VALID_STATUSES[number];
+export const VALID_STATUSES = [
+  "ACT",
+  "INA",
+  "ACTG",
+  "PND",
+  "BAN",
+  "DEL",
+  "EXP",
+  "REV",
+] as const;
+export type CodeStatus = (typeof VALID_STATUSES)[number];
+export const TYPE_BOOKINGS = {
+  HOUR: "HOUR",
+  NIGHT: "NIGHT",
+  DAY: "DAY",
+} as const;
+
+export type TypeBooking = keyof typeof TYPE_BOOKINGS;
--- src/controllers/booking.controller.ts
+++ src/controllers/booking.controller.ts
@@ -6,6 +6,8 @@ import {
 import {
   GetBookingInFoResponse,
   GetRoomsByHotelResponse,
+  Note,
+  Surcharge,
 } from "@/types/response/booking";
 import { catchAsyncErrorWithCode } from "@/utils/catchAsyncError";
 import { Response } from "express";
@@ -43,4 +45,64 @@ export const addBooking = catchAsyncErrorWithCode(
     res.status(200).json(result);
   },
   "ADD_ERROR"
+);
+
+export const addSurcharge = catchAsyncErrorWithCode(
+  async (req: BodyRequest<Surcharge>, res: Response<BaseResponse<null>>) => {
+    const result = await bookingService.AddSurcharge(req);
+    res.status(200).json(result);
+  },
+  "ADD_ERROR"
+);
+
+export const addNote = catchAsyncErrorWithCode(
+  async (
+    req: AuthenticatedRequest<{ id: string }, Note>,
+    res: Response<BaseResponse<null>>
+  ) => {
+    const result = await bookingService.AddNote(req);
+    res.status(200).json(result);
+  },
+  "ADD_ERROR"
+);
+
+export const addUtility = catchAsyncErrorWithCode(
+  async (
+    req: BodyRequest<{
+      utilityId: string;
+      bookingId: string;
+      quantity?: number;
+    }>,
+    res: Response<BaseResponse<null>>
+  ) => {
+    const result = await bookingService.AddUtility(req);
+    res.status(200).json(result);
+  },
+  "ADD_ERROR"
+);
+
+export const removeUtility = catchAsyncErrorWithCode(
+  async (
+    req: BodyRequest<{
+      bookingId: string;
+      utilityId: string;
+      quantity?: number;
+    }>,
+    res: Response<BaseResponse<null>>
+  ) => {
+    const result = await bookingService.RemoveUtilityService(req);
+    res.status(200).json(result);
+  },
+  "DELETE_ERROR"
+);
+
+export const removeBooking = catchAsyncErrorWithCode(
+  async (
+    req: ParamsRequest<{ id: string }>,
+    res: Response<BaseResponse<null>>
+  ) => {
+    const result = await bookingService.removeBooking(req);
+    res.status(200).json(result);
+  },
+  "DELETE_ERROR"
 );
